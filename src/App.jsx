a79bf73ebc6539f918a894d2574a53380e331f6d
Omar Ahmed <omarwork010@gmail.com>
2022-03-25 03:50:37 -0700

    Add Some Features
    - Add To Cart
    - Add to WesihList
    - Validation Forms
    -Calculate All Price
    - Cheack out Page
    - Add Some Functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,141 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import React from "react";
-import Cart from "./pages/cart/Cart";
-import Home from "./pages/home/Home";
-import LogIn from "./pages/logIn/LogIn";
-import ProductList from "./pages/productList/ProductList";
-import ProductPage from "./pages/productPage/ProductPage";
-import Register from "./pages/register/Register";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { useState } from 'react'
+import Cart from './pages/cart/Cart'
+import Home from './pages/home/Home'
+import LogIn from './pages/logIn/LogIn'
+import ProductList from './pages/productList/ProductList'
+import ProductPage from './pages/productPage/ProductPage'
+import Register from './pages/register/Register'
+import Toper from './components/toper/Toper'
+import Navbar from './components/navbar/Navbar'
+import CheckOut from './pages/checkOut/CheckOut'
+import WishList from './pages/wishList/WishList'
 
-function App() {
+function App () {
+  const [user, setUser] = useState(false)
+
+  const [cartItems, setCartItems] = useState([])
+  const [wishList, setWishList] = useState([])
+
+  const onAdd = product => {
+    const exist = cartItems.find(x => x.id === product.id)
+
+    if (exist) {
+      setCartItems(
+        cartItems.map(x =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      )
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }])
+    }
+  }
+
+  const onAddToWishList = product => {
+    const exist = wishList.find(x => x.id === product.id)
+
+    if (exist) {
+      setWishList(
+        wishList.map(x =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      )
+    } else {
+      setWishList([...wishList, { ...product, qty: 1 }])
+    }
+  }
+
+  const onRemove = product => {
+    const exist = cartItems.find(x => x.id === product.id)
+
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter(x => x.id !== product.id))
+    } else {
+      setCartItems(
+        cartItems.map(x =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      )
+    }
+  }
+
+  const itemsPrice = cartItems.reduce((a, b) => a + b.price * b.qty, 0)
+
   return (
     <BrowserRouter>
+      <Toper />
+      <Navbar user={user} cartItems={cartItems} wishList={wishList} />
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/productlist" element={<ProductList />} />
-        <Route path="/productpage" element={<ProductPage />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route
+          path='/'
+          exact
+          element={
+            <Home
+              cartItems={cartItems}
+              setCartItems={setCartItems}
+              onAdd={onAdd}
+              user={user}
+              onAddToWishList={onAddToWishList}
+            />
+          }
+        />
+        <Route
+          path='/login'
+          element={<LogIn user={user} setUser={setUser} />}
+        />
+        <Route path='/register' element={<Register />} />
+
+        <Route
+          path='/productlist'
+          element={
+            <ProductList
+              cartItems={cartItems}
+              setCartItems={setCartItems}
+              onAdd={onAdd}
+              onAddToWishList={onAddToWishList}
+            />
+          }
+        />
+        <Route
+          path='/productlist/:id'
+          element={
+            <ProductPage
+              onAdd={onAdd}
+              onRemove={onRemove}
+              cartItems={cartItems}
+            />
+          }
+        />
+        <Route
+          path='/cart'
+          element={
+            <Cart
+              cartItems={cartItems}
+              onAdd={onAdd}
+              onRemove={onRemove}
+              wishList={wishList}
+              itemsPrice={itemsPrice}
+            />
+          }
+        />
+        <Route
+          path='/checkout'
+          element={
+            <CheckOut
+              cartItems={cartItems}
+              itemsPrice={itemsPrice}
+              onAdd={onAdd}
+              onRemove={onRemove}
+            />
+          }
+        />
+        <Route
+          path='/wishlist'
+          element={<WishList wishList={wishList} onAdd={onAdd} />}
+        />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,82 +1,77 @@
-import styled from "styled-components";
-import { mobile } from "../../responsive";
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { mobile } from '../../responsive'
 
 const Container = styled.div`
-    display: flex;
-    padding: 20px;
+  display: flex;
+  padding: 20px;
 
-    ${mobile({flexDirection: "column"})};
-
-`;
+  ${mobile({ flexDirection: 'column' })};
+`
 
 const Left = styled.div`
   flex: 1;
 
-    ${mobile({marginBottom: "20px"})};
-
-`;
+  ${mobile({ marginBottom: '20px' })};
+`
 
 const Title = styled.h2`
-    color: #333;
-    margin-top: 0;
-    letter-spacing: 2px;
-`;
+  color: #333;
+  margin-top: 0;
+  letter-spacing: 2px;
+`
 
 const Desc = styled.p`
-    color: #444;
+  color: #444;
   line-height: 1.7;
   margin-top: 0;
-`;
+`
 
 const SocialIcons = styled.div`
-    display: flex;
-`;
-
+  display: flex;
+`
 
 const SocialIcon = styled.div`
-    width: 35px;
-    height: 35px;
-    background: #${props => props.color};
-    margin-right: 20px;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white;
-`;
+  width: 35px;
+  height: 35px;
+  background: #${props => props.color};
+  margin-right: 20px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+`
 
 const Center = styled.div`
   flex: 1;
-    ${mobile({display: "none"})};
-
-`;
+  ${mobile({ display: 'none' })};
+`
 
 const List = styled.ul`
-    padding: 0;
-    list-style: none;
-    margin-top: 10px;
-    margin-bottom: 0;
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    height: 50%;
-`;
+  padding: 0;
+  list-style: none;
+  margin-top: 10px;
+  margin-bottom: 0;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  height: 50%;
+`
 
 const ListItem = styled.li`
-    margin-bottom: 15px;
-`;
+  margin-bottom: 15px;
 
-const Link = styled.a`
+  a {
     color: #444;
     font-weight: bold;
     text-decoration: none;
-`;
-
+  }
+`
 
 const Right = styled.div`
   flex: 1;
-
-`;
+`
 
 const ContactItem = styled.div`
     margin-bottom: 20px;
@@ -84,86 +79,86 @@ const ContactItem = styled.div`
     display: flex;
     align-items: center;
 }
-`;
+`
 
-const Payment = styled.img`
+const Payment = styled.img``
 
-`;
-
-
-export default function Footer() {
+export default function Footer () {
   return (
-      <Container>
-          <Left>
-            <Title>E-Commerce App</Title>
-            <Desc>
-              There are many variations of passages of Lorem Ipsum available, but
-              the majority have suffered alteration in some form, by injected
-              humour, or randomised words which don’t look even slightly believable.
-            </Desc>
-            <SocialIcons>
-               <SocialIcon color="3B5999">
-                  <i className="fab fa-facebook"></i>
-                </SocialIcon>
-                <SocialIcon color="E4405F">
-                    <i className="fab fa-instagram"></i>
-                </SocialIcon>
-                <SocialIcon color="55ACEE">
-                    <i className="fab fa-twitter"></i>
-                </SocialIcon>
-                <SocialIcon color="E60023">
-                    <i className="fab fa-pinterest"></i>
-                </SocialIcon>
-            </SocialIcons>
-          </Left>
-          <Center>
-            <Title>Useful Links</Title>
-            <List>
-              <ListItem>
-                <Link href="#" >Home</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#" >Cart</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#" >Man Fashion</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#" >Woman Fashion</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#" >Accessories</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#" >My Account</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#" >Order Tracking</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#" >Wishlist</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#" >Teams</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#" >Links</Link>
-              </ListItem>
-            </List>
-          </Center>
-          <Right>
-            <Title>Contact</Title>
-            <ContactItem>
-              <i class="fas fa-at"  style={{marginRight:"10px"}}></i> Alexandria , El-Mandara
-            </ContactItem>
-            <ContactItem>
-              <i className="fas fa-phone"  style={{marginRight:"10px"}}></i> +20 106 046 298
-            </ContactItem>
-            <ContactItem>
-              <i className="fas fa-envelope"  style={{marginRight:"10px"}}></i> omarwork010@gmail.com
-            </ContactItem>
-            <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
-          </Right>
-      </Container>
-  );
+    <Container>
+      <Left>
+        <Title>E-Commerce App</Title>
+        <Desc>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don’t look even slightly believable.
+        </Desc>
+        <SocialIcons>
+          <SocialIcon color='3B5999'>
+            <i className='fab fa-facebook'></i>
+          </SocialIcon>
+          <SocialIcon color='E4405F'>
+            <i className='fab fa-instagram'></i>
+          </SocialIcon>
+          <SocialIcon color='55ACEE'>
+            <i className='fab fa-twitter'></i>
+          </SocialIcon>
+          <SocialIcon color='E60023'>
+            <i className='fab fa-pinterest'></i>
+          </SocialIcon>
+        </SocialIcons>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>
+            <Link to='/'>Home</Link>
+          </ListItem>
+          <ListItem>
+            <Link to='/cart'>Cart</Link>
+          </ListItem>
+          <ListItem>
+            <Link to='/productlist'>Man Fashion</Link>
+          </ListItem>
+          <ListItem>
+            <Link to='/productlist'>Woman Fashion</Link>
+          </ListItem>
+          <ListItem>
+            <Link to='/'>Accessories</Link>
+          </ListItem>
+          <ListItem>
+            <Link to='/'>My Account</Link>
+          </ListItem>
+          <ListItem>
+            <Link to='/'>Order Tracking</Link>
+          </ListItem>
+          <ListItem>
+            <Link to='/'>Wishlist</Link>
+          </ListItem>
+          <ListItem>
+            <Link to='/'>Teams</Link>
+          </ListItem>
+          <ListItem>
+            <Link to='/'>Links</Link>
+          </ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <i className='fas fa-at' style={{ marginRight: '10px' }}></i>{' '}
+          Alexandria , El-Mandara
+        </ContactItem>
+        <ContactItem>
+          <i className='fas fa-phone' style={{ marginRight: '10px' }}></i> +20
+          106 046 298
+        </ContactItem>
+        <ContactItem>
+          <i className='fas fa-envelope' style={{ marginRight: '10px' }}></i>{' '}
+          omarwork010@gmail.com
+        </ContactItem>
+        <Payment src='https://i.ibb.co/Qfvn4z6/payment.png' />
+      </Right>
+    </Container>
+  )
 }
